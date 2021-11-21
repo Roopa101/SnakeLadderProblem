41d6c48ade998c43d26a6204991dda3514e83e8e
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-21 22:52:47 +0530

    [Roopa] Checking the option Noplay,Ladder,Snake

--- SnakeLadderProgram/SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -9,13 +9,37 @@ namespace SnakeLadderProgram
     class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             const int InitialPosition = 0;
+            const int LadderPosition = 1;
+            const int SnakePosition = 2;
+
             int SinglePlayerPosition = InitialPosition;
-            Console.WriteLine("Single Player is starting with position" + SinglePlayerPosition);
+         
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
             Console.WriteLine("Player got the num :" + DiceRoll);
+            Random random1 = new Random();
+            int CheckForoptions = random.Next(0, 3);
+
+            switch (CheckForoptions)
+            {
+                case LadderPosition:
+                    SinglePlayerPosition = SinglePlayerPosition + DiceRoll;
+                    Console.WriteLine("Player got a Ladder , player position is + " SinglePlayerPosition);
+                    break;
+
+                case SnakePosition:
+                    SinglePlayerPosition = SinglePlayerPosition - DiceRoll;
+                    Console.WriteLine("Player got a Snake , player position is + " SinglePlayerPosition);
+                    break;
+
+                default:
+                    Console.WriteLine(""Player got nothing ");
+                    break;
+
+            }
+            
             Console.ReadLine();
         }
     }
