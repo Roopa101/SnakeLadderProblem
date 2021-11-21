ee382d500f9c7c06e54d90651ce946ab59dfecb6
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-21 23:22:54 +0530

    [Roopa] player reaches the winning posion 100

--- SnakeLadderProgram/SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -11,31 +11,33 @@ namespace SnakeLadderProgram
         public static void Main(string[] args)
         { 
             const int InitialPosition = 0;
+            const int FinalPosition = 100;
             const int LadderPosition = 1;
             const int SnakePosition = 2;
 
             int SinglePlayerPosition = InitialPosition;
-         
+
+            while (SinglePlayerPosition <= FinalPosition) 
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
             Console.WriteLine("Player got the num :" + DiceRoll);
             Random random1 = new Random();
-            int CheckForoptions = random.Next(0, 3);
+            int CheckForoptions = random1.Next(0, 3);
 
             switch (CheckForoptions)
             {
                 case LadderPosition:
                     SinglePlayerPosition = SinglePlayerPosition + DiceRoll;
-                    Console.WriteLine("Player got a Ladder , player position is + " SinglePlayerPosition);
+                    Console.WriteLine("Player got a Ladder , player position is  " + SinglePlayerPosition);
                     break;
 
                 case SnakePosition:
                     SinglePlayerPosition = SinglePlayerPosition - DiceRoll;
-                    Console.WriteLine("Player got a Snake , player position is + " SinglePlayerPosition);
+                    Console.WriteLine("Player got a Snake , player position is  " + SinglePlayerPosition);
                     break;
 
                 default:
-                    Console.WriteLine(""Player got nothing ");
+                    Console.WriteLine("Player got nothing " + SinglePlayerPosition);
                     break;
 
             }
