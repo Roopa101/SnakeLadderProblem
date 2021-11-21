0fc0b33bdd1c1d4f45c81b601c0a5a5427d57e74
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-22 00:59:04 +0530

    [Roopa] Calculating the numnber of times the dice was played to win the game

--- SnakeLadderProgram/SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -8,63 +8,68 @@ namespace SnakeLadderProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             const int InitialPosition = 0;
-            const int WinningPosition = 100;
-            const int Ladder = 1;
-            const int Snake = 2;
-            int PlayerPosition = InitialPosition;
-
-            while (PlayerPosition <= WinningPosition)
+            const int FinalPosition = 100;
+            const int LadderPosition = 1;
+            const int SnakePosition = 2;
+            int SinglePlayerPosition = InitialPosition;
+            int count = 0;
+            while (SinglePlayerPosition <= FinalPosition)
             {
                 Random random = new Random();
                 int DiceRoll = random.Next(1, 7);
-                Console.WriteLine("Player got the number on dice is: " + DiceRoll);
+                //To track the numbers of loop ran till winning, we have to assign an incremental variable within the loop
+                count++;
+                Console.WriteLine("Player got the num : " + DiceRoll);
                 Random random2 = new Random();
-                int Checkoptions = random.Next(0, 3);
-                switch (Checkoptions)
+                int CheckForOptions = random.Next(0, 3);
+                switch (CheckForOptions)
                 {
-                    case Ladder:
-                        PlayerPosition = PlayerPosition + DiceRoll;
+                    case LadderPosition:
+                        SinglePlayerPosition = SinglePlayerPosition + DiceRoll;
                         //Here to prevent the player position to surpass the winning position we have to give the if condition.
-                        if (PlayerPosition > WinningPosition)
+                        if (SinglePlayerPosition > FinalPosition)
                         {
-                            PlayerPosition = PlayerPosition - DiceRoll;
-                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                            SinglePlayerPosition = SinglePlayerPosition - DiceRoll;
+                            Console.WriteLine("Player got Ladder. Player position is :" + SinglePlayerPosition);
                         }
                         //Here if player reaches to 100, we have to declare him the winner.
-                        else if (PlayerPosition == WinningPosition)
+                        else if (SinglePlayerPosition == FinalPosition)
                         {
-                            Console.WriteLine("Player reached " + PlayerPosition + ". Player Won the Game");
+                            Console.WriteLine("Player reached " + SinglePlayerPosition + ". Player Won the Game");
+                            //To Count the number of times the dice was played
+                            //We have to write the incremental variable which we used to track the numbers of iterations performed by a loop.
+                            Console.WriteLine("Number of times the player rolled the dice is :" + count);
+                            Console.ReadLine();
                             //To end the program here we are using return. 
                             return;
                         }
                         else
                         {
-                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                            Console.WriteLine("Player got Ladder. Player position is :" + SinglePlayerPosition);
                         }
                         break;
-                    case Snake:
-                        PlayerPosition = PlayerPosition - DiceRoll;
+                    case SnakePosition:
+                        SinglePlayerPosition = SinglePlayerPosition - DiceRoll;
                         //Here we have to give the if condition so that player position doesn't goto negative value.
-                        if (PlayerPosition < InitialPosition)
+                        if (SinglePlayerPosition < InitialPosition)
                         {
-                            PlayerPosition = InitialPosition;
-                            Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
+                            SinglePlayerPosition = InitialPosition;
+                            Console.WriteLine("Player got Snake. Player position is :" + SinglePlayerPosition);
                         }
                         //Here if the player position is not negative player position is usually updated.
                         else
                         {
-                            Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
+                            Console.WriteLine("Player got Snake. Player position is :" + SinglePlayerPosition);
                         }
                         break;
                     default:
-                        Console.WriteLine("Player got Nothing. Player position is :" + PlayerPosition);
+                        Console.WriteLine("Player got Nothing. Player position is :" + SinglePlayerPosition);
                         break;
                 }
             }
-            Console.ReadLine();
-
         }
     }
+}
