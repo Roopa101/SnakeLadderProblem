3795a697f34056c7172925896e8aa0bc328769ca
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-22 00:45:02 +0530

    [Roopa] checking the player got Exact 100 position for winning

--- SnakeLadderProgram/SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -8,41 +8,63 @@ namespace SnakeLadderProgram
 {
     class Program
     {
-        public static void Main(string[] args)
-        { 
+        static void Main(string[] args)
+        {
             const int InitialPosition = 0;
-            const int FinalPosition = 100;
-            const int LadderPosition = 1;
-            const int SnakePosition = 2;
+            const int WinningPosition = 100;
+            const int Ladder = 1;
+            const int Snake = 2;
+            int PlayerPosition = InitialPosition;
 
-            int SinglePlayerPosition = InitialPosition;
-
-            while (SinglePlayerPosition <= FinalPosition) 
-            Random random = new Random();
-            int DiceRoll = random.Next(1, 7);
-            Console.WriteLine("Player got the num :" + DiceRoll);
-            Random random1 = new Random();
-            int CheckForoptions = random1.Next(0, 3);
-
-            switch (CheckForoptions)
+            while (PlayerPosition <= WinningPosition)
             {
-                case LadderPosition:
-                    SinglePlayerPosition = SinglePlayerPosition + DiceRoll;
-                    Console.WriteLine("Player got a Ladder , player position is  " + SinglePlayerPosition);
-                    break;
-
-                case SnakePosition:
-                    SinglePlayerPosition = SinglePlayerPosition - DiceRoll;
-                    Console.WriteLine("Player got a Snake , player position is  " + SinglePlayerPosition);
-                    break;
-
-                default:
-                    Console.WriteLine("Player got nothing " + SinglePlayerPosition);
-                    break;
-
+                Random random = new Random();
+                int DiceRoll = random.Next(1, 7);
+                Console.WriteLine("Player got the number on dice is: " + DiceRoll);
+                Random random2 = new Random();
+                int Checkoptions = random.Next(0, 3);
+                switch (Checkoptions)
+                {
+                    case Ladder:
+                        PlayerPosition = PlayerPosition + DiceRoll;
+                        //Here to prevent the player position to surpass the winning position we have to give the if condition.
+                        if (PlayerPosition > WinningPosition)
+                        {
+                            PlayerPosition = PlayerPosition - DiceRoll;
+                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                        }
+                        //Here if player reaches to 100, we have to declare him the winner.
+                        else if (PlayerPosition == WinningPosition)
+                        {
+                            Console.WriteLine("Player reached " + PlayerPosition + ". Player Won the Game");
+                            //To end the program here we are using return. 
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                        }
+                        break;
+                    case Snake:
+                        PlayerPosition = PlayerPosition - DiceRoll;
+                        //Here we have to give the if condition so that player position doesn't goto negative value.
+                        if (PlayerPosition < InitialPosition)
+                        {
+                            PlayerPosition = InitialPosition;
+                            Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
+                        }
+                        //Here if the player position is not negative player position is usually updated.
+                        else
+                        {
+                            Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Player got Nothing. Player position is :" + PlayerPosition);
+                        break;
+                }
             }
-            
             Console.ReadLine();
+
         }
     }
-}
