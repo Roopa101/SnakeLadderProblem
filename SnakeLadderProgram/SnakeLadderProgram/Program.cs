c07d574bb5a9862772e2626b1beab9f01ec8d574
Roopa101 <83457806+Roopa101@users.noreply.github.com>
2021-11-21 21:53:53 +0530

    [Roopa] Player got a random dice number

--- SnakeLadderProgram/SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -10,6 +10,9 @@ namespace SnakeLadderProgram
     {
         public static void Main(string[] args)
         {
+            const int InitialPosition = 0;
+            int SinglePlayerPosition = InitialPosition;
+            Console.WriteLine("Single Player is starting with position" + SinglePlayerPosition);
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
             Console.WriteLine("Player got the num :" + DiceRoll);
